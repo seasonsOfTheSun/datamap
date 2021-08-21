792d49d2efa8a8986c631927d661d8f72d2a4e4d
Joel Hancock <joel@menchelab.com>
2021-08-21 17:31:51 +0200

    utility script to join toogether multiple outpusts to a single .csv

---
+++ src/zip.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import numpy as np
+
+def join_files(foldername):
+    out = []
+    for i in os.listdir(foldername):
+        temp = pd.read_csv(foldername +"/"+ i, index_col = 0, header = None)
+        temp = temp[1]
+        out.append(temp)
+
+    index = set([]).union(*[set(i.index) for i in out])
+    temp = [temp.reindex(list(index)) for temp in out]
+    df = pd.concat(out, axis = 1).transpose()
+    return df
+
+if __name__ == "__main__":
+   
+   import sys
+   foldername = sys.argv[1]
+   df = join_files(foldername)
+   df.to_csv(foldername + ".csv")
